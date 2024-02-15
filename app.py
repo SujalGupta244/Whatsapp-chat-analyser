f23d6168a4b3f180226b83efdcc6df0062cebf34
sujal gupta <sujalgupta5656@gmail.com>
2024-02-15 19:34:31 +0530

    initialising the whatsapp-chat-analyser project

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("Hello world")
+
+st.sidebar.title("Whatsapp Chat Analyser")
---
+++ preprocessor.py
@@ -0,0 +1,39 @@
+import re
+import pandas as pd
+
+
+def preprocessor(data):
+    patterns = '\d{2}/\d{2}/\d{4},\s\d{1,2}:\d{2}\u202f?[apmAPM]{2}\s-\s'
+    messages = re.split(patterns, data)[1:]
+    dates = re.findall(patterns, data)
+
+    df = pd.DataFrame({'user_message':messages, 'message_date': dates})
+    # convert messages_data type
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %I:%M %p - ')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+
+    # separate users and messages
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s',message)
+        print(entry)
+        if(entry[1:]): # user name
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    return df
